cd64d7288db3b9d4f8c8f684af083ea1a0493706
Gr4y-r0se <59868499+Gr4y-r0se@users.noreply.github.com>
2022-02-04 11:30:05 +0000

    added CIDR breakdown
    Includes the IPAddr gem now to break down CIDR notation and provide a better picture of the ips that are live

--- modules/online.rb
+++ modules/online.rb
@@ -1,4 +1,6 @@
 require "Nokogiri"
+require 'rubygems'
+require 'ipaddr'
 
 require_relative "targets"
 
@@ -10,17 +12,24 @@ def online(file_)
 	file = File.open(file_) 
 
 	doc = Nokogiri::XML(file)
-
+	regex = "/^([01]?\d\d?|2[0-4]\d|25[0-5])(?:\.(?:[01]?\d\d?|2[0-4]\d|25[0-5])){3}(?:/[0-2]\d|/3[0-2])?$/"
 	lst = doc.xpath("//preference/name[text() = 'TARGET']")
-	targets_list = lst.xpath('../value/text()').to_s.split(',').each
+	targets_list = lst.xpath('../value/text()').to_s.split(',')
  	targets_list.each do |i|
- 		if i.match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/) then
- 			target_ips.append(i)
+ 		if i.match(/^([01]?\d\d?|2[0-4]\d|25[0-5])(?:\.(?:[01]?\d\d?|2[0-4]\d|25[0-5])){3}(?:\/[0-2]\d|\/3[0-2])?$/) then
+ 			if i.include? '/' then 
+ 				values = IPAddr.new(i).to_range()
+ 				values.each do |ip|
+ 					target_ips.append(ip.to_s)
+ 				end
+ 			else
+ 				target_ips.append(i)
+ 			end
  		else
  			target_fqdns.append(i)
  		end
  	end
- 	target_ips.sort_by! {|ip| ip.split('.').map{ |octet| octet.to_i} }
+ 	target_ips.uniq.sort_by! {|ip| ip.split('.').map{ |octet| octet.to_i} }
  	target_fqdns = target_fqdns.sort
 
 
