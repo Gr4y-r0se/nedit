32b91d472cf1ac14f9820f6e074fa1a3dc3c41f1
Gr4y-r0se <59868499+Gr4y-r0se@users.noreply.github.com>
2024-02-22 17:51:03 +0000

    Added compliance check spreadsheet generation

--- modules/csv.rb
+++ modules/csv.rb
@@ -1,9 +1,41 @@
 require "nokogiri"
 
+def compliance_checks(xml,compliance_name)
+	checks = {}
+	path_ = '//ReportItem[@pluginName="'+compliance_name+'"]'
+	compliance_lst = xml.xpath(path_)
+
+	# Order is: Result, Check_Name, Benchmark_Name, Check_Info, Check_Result, See_Also
+
+	for i in compliance_lst do 
+		checks[i.xpath('.//cm:compliance-check-name/text()', {'cm' => 'http://www.nessus.org/cm'}).to_s] = [
+			i.xpath('.//cm:compliance-result/text()', {'cm' => 'http://www.nessus.org/cm'}).to_s,
+			i.xpath('.//cm:compliance-benchmark-name/text()', {'cm' => 'http://www.nessus.org/cm'}).to_s,
+			i.xpath('.//cm:compliance-info/text()', {'cm' => 'http://www.nessus.org/cm'}).to_s,
+			i.xpath('.//cm:compliance-actual-value/text()', {'cm' => 'http://www.nessus.org/cm'}).to_s.gsub('"','""')[...6000],
+			i.xpath('.//cm:compliance-see-also/text()', {'cm' => 'http://www.nessus.org/cm'}).to_s
+		]
+	end
+	
+
+	writeable = ['"Compliance Check Title","Result","Benchmark Name","Description","Details","See Also"']
+
+	checks.each do |title, values|
+		writeable.push(%Q("#{title}","#{values[0]}","#{values[1]}","#{values[2]}","#{values[3]}","#{values[4]}"))
+	end
+
+	return writeable
+end
+
 
 def csv(file_)
 	vulns = {}
 	file = File.open(file_) 
+	
+	#For detecting Compliance Checks
+	compliance = false
+	check_name = ''
+	checks_filename = ''
 
 	doc = Nokogiri::XML(file)
 	full = doc.root
@@ -12,8 +44,24 @@ def csv(file_)
 	#My gin addled brain needed this so I could make sense of the nested hash value:
 	# [0 - CVE],[1 - CVSS 3 score],[2 - Risk/Severity],[3 - Ip address],[4 - Protocol],[5 - Port],[6- Description],[7 - Remediation],[8 - External References],[9 - plugin output],[10 - hostname],[11 - rdns]
 
-	for i in lst do vulns[i['pluginName']] ||= [[],[],[],[],[],[],[],[],[],[],[],[]] if i['severity'] !=  '0' end  #This declares a default value for all the vulnerabilites, which we will later populate
-	 
+	#This declares a default value for all the vulnerabilites, which we will later populate
+	for i in lst do 
+		if i['pluginName'].include? 'Compliance Checks' then
+			compliance = true
+			check_name = i['pluginName']
+		elsif i['severity'] !=  '0' then
+			vulns[i['pluginName']] ||= [[],[],[],[],[],[],[],[],[],[],[],[]]  
+		end  
+	end
+	
+	if compliance == true then
+		puts "\n	Detected #{check_name} - Generating Additional Spreadsheet."
+		csv_structure = compliance_checks(doc,check_name)
+		
+		checks_filename = "#{file_.rpartition('.')[0]}_compliance-results.csv"
+
+		File.write(checks_filename,csv_structure.join("\n"))
+	end
 
 	vulns.each do |vuln , values|
 		path_ = '//ReportItem[@pluginName="'+vuln+'"]'
@@ -85,9 +133,9 @@ def csv(file_)
 	end
 
 	File.write(filename,writeable.join("\n"))
-
+	if checks_filename != '' then
+		filename = filename + " and #{checks_filename}"
+	end
 	return filename
 end
-
-
 
